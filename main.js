c95e84b82b5801697b47428000f523fdd7943eef
kevinqq <kevin_1321124136@uptecamac.edu.mx>
2021-09-27 11:36:52 -0500

    repasar

--- main.js
+++ main.js
@@ -1,9 +1,238 @@
-var x = 10;
-var y = 20;
-var s = 30;
-if (x < y){
-    console.log(x + " es menor a " + y)
+//var x = 10;
+//var y = 20;
+//var s = 30;
+//if (x < y){
+//    console.log(x + " es menor a " + y);
 
-}else{
-    console.log(z +" es mayot a " +y)
+//}else{
+//    console.log(z +" es mayot a " +y);
+//}
+
+//var humano1 = "kevim";
+//var humano2  = "Pedro";
+
+//if(humano1 == humano2){
+  //  console.log("son iguales");
+//}else{
+  //  console.log("son diferentes");
+//} 
+
+//var tortilleria = "tortilllas";
+
+//console.log("Paso 1: Caminar hacia la tortilleria");
+
+//if(tortilleria == "tortillas"){
+  //  console.log("Paso 2: Pedir tortillas");
+
+//}else{
+  //  console.log("Paso 2: Si no tiene ir a la tienda");
+
+//}
+
+//console.log("Paso 3: pagar las torillas");
+
+//if(tortilleria =="totillas"){
+  //  console.log("Paso 4: Regresar caminando");
+//}else{
+  //  console.log("Paso 4: regresar en combi si te sobra cambio");
+//}
+//console.log("Paso 5: llegar a tu casa");
+//console.log("Paso 6: darle el cambio a tu mama");
+//console.log("paso 7: agradecer que esta viva");
+
+//var operacion = "suma" ;
+//var n1 = 10;
+//var n2 = 5;
+//switch(operación){
+  //case "suma":
+  //case "resta":
+    //console.log("la operación de la resta es: ");
+    //break;
+    //case "multiplicacion":
+    //console.log("la operación de la multiplicacion es: ");
+    //break;
+    //case "division":
+    //console.log("la operación de la division es: ");
+   // break;
+    //default:
+    //  console.log("No se reconoce la operacion");
+  //  break;
+//}
+
+
+
+
+//var n = 1;
+
+//if(n > 10){
+
+  //  console.log("compra un dulce");
+
+//}else{
+
+  //  switch(n){
+
+    //    case 1:
+
+      //      console.log(1);
+
+        //break;
+
+        //case 2:
+
+          //  console.log(2);
+
+        //break;
+
+        //case 3:
+
+          //  console.log(3);
+
+        //break;
+
+        //case 4:
+
+          //  console.log(4);
+
+        //break;
+
+        //default:
+
+          //  console.log("Es menor a 1");
+
+        //break;
+
+    //}
+
+//}
+
+
+//var tortilleria = "tortilllas";
+
+//console.log("Paso 1: Caminar hacia la tortilleria");
+
+//if(tortilleria == "tortillas"){
+  //  console.log("Paso 2: Pedir tortillas");
+
+//}else{
+   // console.log("Paso 2: Si no tiene ir a la tienda");
+
+//}
+
+
+
+//*var numero = "11";
+//console.log("vas a ir a la tienda");
+//if(numero > "10"){
+//console.log("compra chetos");
+//}else{
+  //console.log("no te alcanza");
+//}
+
+//var nombre = "pedro";
+//console.log("¿Quien es tu mejor amigo?");
+//if(nombre == "pedro"){
+//console.log("tu amigo es pedro");
+//}else{
+  //console.log("no tengo amigos");
+//}
+
+//var tarea = "trabaja"
+//var produccion = "si";
+//var morir = "no";
+
+//console.log("¿Quieres pasar la universidad?");
+//if(produccion == tarea) {
+//console.log("me voy a poner a hacer tarea");
+//}else{
+//console.log("mejor vendo bonices");
+//}
+//var tal = "tarea"
+//var num1 = "Si";
+//var num2 = "no";
+//console.log("Voy a ver un partido del america");
+//if(num1 == tal){
+//console.log("No podemos camepon ay tarea");
+//}else{
+//console.log("Con toda confianza grita gol");
+//}
+//var num1 = "tercera edad";
+//var num2 = "15";
+//var num3 = "30"
+//console.log("Quien es mas viejo");
+//switch(num1){
+  //case "tercera edad":
+    //if(30 > 15 ){
+      //console.log("eres de la tercera edad");
+    //}else{
+      //console.log("aun eres joven");
+    //}
+  //  break;
+//}
+
+//for(var x=5; x>=20; x+=3){
+ 
+//}
+//EJERCICIO 1
+//var x = 3 ;
+//while(x <=10 ){
+//console.log(x);
+//x++;
+//}
+
+
+//EJERCICIO 2
+//var x = 100;
+//while(x <= 199){
+//console.log(x);
+//x++:
+//}
+
+
+//EJERCICIO 3
+//let num = 5;
+//while(true){
+//if(num>=23){
+  // break;
+  //}
+  //console.log(num);
+  //num +=3;
+//}
+//EJERCICIO 4
+//var num = parseInt(prompt("Ingresa la camtidad de numeros que gustes brother"));
+//console.log("tu numero ingresado es" +num);
+//for(i= num; i <=num; i++){
+  //console.log( i*2-1);
+//}
+
+
+//EJERCICIO 5
+//var total = 0;
+//var num = parseInt(prompt("Ingresa la camtidad de numeros que gustes brother"));
+//for(var i=1; i<=num; i++){
+//var num2= parseInt(prompt("ingresa al numero al sumar"));
+//console.log(total+=num2);
+//}
+
+//EJERCICIO 8
+
+//for(var x = 1; x <=100; x++){
+  //for(var y = 1; y <=100; y++){
+    //console.log(x + "+" +y+ "=" );
+    //console.log(x+y);
+  //}
+//}
+
+let num = 0;
+while(true){
+if(num>=100){
+   break;
+  }
+  console.log(num);
+  console.log()
+  num +=3;
 }
+
+
+
+
