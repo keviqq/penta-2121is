e46e2227fff2fd84b6b50805dd0d2fb077df5d40
kevinqq <kevin_1321124136@uptecamac.edu.mx>
2021-09-27 22:01:25 -0500

    ejercicios dobles

--- ejemplo2.js
+++ ejemplo2.js
@@ -1,13 +1,15 @@
-//*var numero = "11";
-//console.log("vas a ir a la tienda");
-//if(numero > "10"){
-//console.log("compra chetos");
-//}else{
-  //console.log("no te alcanza");
-//}
+//ejercicios con if
 
-//2
+//ejercicio 1
+var numero = "11";
+console.log("vas a ir a la tienda");
+if(numero > "10"){
+console.log("compra chetos");
+}else{
+  console.log("no te alcanza");
+}
 
+//ejercicio 2
 //var nombre = "pedro";
 //console.log("¿Quien es tu mejor amigo?");
 //if(nombre == "pedro"){
@@ -16,12 +18,10 @@
   //console.log("no tengo amigos");
 //}
 
-//3
-
+//ejrcicio 3
 //var tarea = "trabaja"
 //var produccion = "si";
 //var morir = "no";
-
 //console.log("¿Quieres pasar la universidad?");
 //if(produccion == tarea) {
 //console.log("me voy a poner a hacer tarea");
@@ -29,8 +29,8 @@
 //console.log("mejor vendo bonices");
 //}
 
-//4
 
+//ejercicio 4
 //var tal = "tarea"
 //var num1 = "Si";
 //var num2 = "no";
@@ -40,6 +40,78 @@
 //}else{
 //console.log("Con toda confianza grita gol");
 //}
+
+//ejercicio 5
+//var num1 = "18";
+//var num2 = "30";
+//console.log("¿Puedes comprar alchol?");
+//if(30>18){
+  //console.log("puedes comprar alchol");
+//}else{
+  //console.log("estudia mejor chamaco");
+//}
+
+
+//ejercicio con swtich y if
+
+//ejercicio 1
+var adelgazar ="gordito";
+var clima = "frio";
+console.log("que vamos a hacer hoy");
+switch(adelgazar){
+case "gordito":
+  if(clima=="calor"){
+console.log("hace un buen tiempor para hacer ejercicios");
+  }else{
+    console.log("quedate en casa hace frio bro no merece la pena");
+
+  }
+break;
+}
+
+//ejercicio 2
+//var ideas ="menu";
+//var opc = "pollo";
+//console.log("comida para hoy");
+//switch(ideas){
+//case "gordito":
+  //if(opc=="calor"){
+//console.log("hoy solo ay pollo con arroz");
+  //}else{
+    //console.log("pide una pizza bro");
+  //}
+//break;
+//}
+
+//ejercicio 3
+//var BKB ="la unica";
+//var kk = "elite";
+//console.log("que vamos a hacer hoy");
+//switch(BKB){
+//case "la unica ":
+  //if(BKB=="la unica"){
+//console.log("vamos a ver BETTER CALL SAUL");
+  //}else{
+   // console.log("Ve elite o duermete no desperdicies tu time");
+ // }
+//break;
+//}
+
+//ejercicio 4
+//var op1 ="pelear";
+//var op2 = "huir";
+//console.log("un chakalon te quiere navajear");
+//switch(op1){
+//case "pelear":
+  //console.log("moriste de un navajazo ");
+  //}else{
+    //console.log("Un dia mas en latam");
+
+  //}
+//break;
+//}
+
+//ejercicio 5
 //var num1 = "tercera edad";
 //var num2 = "15";
 //var num3 = "30"
@@ -52,4 +124,50 @@
       //console.log("aun eres joven");
     //}
   //  break;
+//}
+
+//ejercicios con switch
+//ejercicio 1
+
+var nomnbre ="juan";
+switch(nomnbre){
+  case "juan":
+  console.log("juan esta soltero aprovechalo :3");
+  break;
+}
+
+//ejercicio 2
+
+//var comer ="tacos";
+//switch(comer){
+  //case "tacos":
+  //console.log("los tacos son nutritivos");
+  //break;
+//}
+
+//ejercicio 3
+
+//var libre ="jugar";
+//switch(libre){
+  //case "jugar":
+  //console.log("jugar friii faller");
+  //break;
+//}
+
+//ejercicio 4
+
+//var problemas ="no entender";
+//switch(problemas){
+  //case "jno entender":
+  //console.log("Toma un curso de programacion");
+  //break;
+//}
+
+//ejercicio 5
+
+//var dor ="mir";
+//switch(dor){
+  //case "mir":
+  //console.log("duerme lo mas que puedas bro");
+  //break;
 //}
